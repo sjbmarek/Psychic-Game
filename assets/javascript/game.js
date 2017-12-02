fd36ffb59a8e9e37e443b2b0fa61a33b1d892a88
Susan Marek <smarek@chaskamn.com>
2017-12-01 22:35:32 -0600

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,45 +4,42 @@
     var losses = 0;
     var guessNumber = 10;
     var yourGuesses =[]
-    var userGuess
-    var computerGuess
+    // var userGuess
+    // var computerGuess
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
     // Loop through 10 times which is the max number of guesses.
-// for (i=1;i=10;i++){
+for (i=1;i<10;i++){
     // This function is run whenever the user presses a key.
   document.onkeyup = function(event) {
-        // console.log('event:  ', event);
-        // userText.textContent=event.key;
-
-      // Determines which key was pressed.
       var userGuess = event.key;
-
-      // Randomly chooses a choice from the options array. 
-      var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
 
       // This is the user guess
       alert("You guessed: " + userGuess);
 
-
-      // // Alerts the Computer's guess.
+      //Alerts the Computer's guess.  REmove later.
       alert("Computer guess: " + computerGuess);
-
-
+      //Winner
       if (userGuess===computerGuess){
         wins++;
         i=10;
+      //Loser
       }
       else {guessNumber=guessNumber-1;
+          //Adds guess to the array
+          console.log(guessNumber);
           yourGuesses.push(userGuess);
-          if(guessNumber=0){losses++}
+          if(guessNumber=0){losses++
+          }
       }
+
     
 
      console.log("wins  " + wins);
      console.log("losses  " + losses);
      console.log("guessNumber  " + guessNumber);
      console.log("yourGuesses  " + yourGuesses);
+     console.log (i);
 
 
     var html=
@@ -50,11 +47,12 @@
       "<p>Computer choose: " + computerGuess + "</p>" +
       "<p>Wins: " + wins + "</p>" +
       "<p>Loses: " + losses + "</p>" +
-      "<p>Number of Guesses Remaining: " + guessNumber + "</p>";
+      "<p>Number of Guesses Remaining: " + guessNumber + "</p>"
+      "<p>Letters you have already guessed: " + yourGuesses + "</p>";
 
       document.querySelector("#game").innerHTML = html;
 
-  // }
+  }
 }
 
     
