2c5b8149f63e70d148aaceeaa66b4d55da886079
Susan Marek <smarek@chaskamn.com>
2017-12-02 19:18:37 -0600

    first successfull game here

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,69 @@
- // Creates an array off all letter choices.
+ // Creates an array of all letter choices.
     var computerChoices = ["a","b","c","d","e","f","g","h","i","j","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     var wins = 0;
     var losses = 0;
     var guessNumber = 10;
     var yourGuesses =[]
-    // var userGuess
-    // var computerGuess
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    
+    console.log (computerGuess);
 
-    // Loop through 10 times which is the max number of guesses.
-for (i=1;i<10;i++){
     // This function is run whenever the user presses a key.
-  document.onkeyup = function(event) {
+      document.onkeyup = function(event) {
       var userGuess = event.key;
+      console.log (userGuess);
 
       // This is the user guess
       alert("You guessed: " + userGuess);
 
-      //Alerts the Computer's guess.  REmove later.
-      alert("Computer guess: " + computerGuess);
-      //Winner
+
+      //Winner, computer picks a new letter, wins increment
       if (userGuess===computerGuess){
         wins++;
-        i=10;
-      //Loser
+        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        console.log (computerGuess);
       }
-      else {guessNumber=guessNumber-1;
-          //Adds guess to the array
+
+      //Keep guessing and array the guesses
+      
+      else if ((userGuess!==computerGuess)&&(guessNumber!==1)){
+          guessNumber--;
           console.log(guessNumber);
+          //Adds guess to the array
           yourGuesses.push(userGuess);
-          if(guessNumber=0){losses++
-          }
+          console.log("yourGuesses  " + yourGuesses);
       }
+      // Loser - When limit is reached, pick a new letter, increment losses, reset icrementer, reset array
+      else if ((userGuess!==computerGuess)&&(guessNumber=1)){
+          losses++;
+          console.log(guessNumber);
+          computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]; 
+          console.log (computerGuess);
+          guessNumber=10;
+          yourGuesses =[];
 
+        
+      }
     
-
-     console.log("wins  " + wins);
-     console.log("losses  " + losses);
-     console.log("guessNumber  " + guessNumber);
-     console.log("yourGuesses  " + yourGuesses);
-     console.log (i);
+       console.log("----------------");
+       console.log("wins  " + wins);
+       console.log("losses  " + losses);
+       console.log("guessNumber  " + guessNumber);
+       console.log("yourGuesses  " + yourGuesses);
+     
 
 
     var html=
-      "<p>You choose: " + userGuess + "</p>" +
-      "<p>Computer choose: " + computerGuess + "</p>" +
+      // "<p>You choose: " + userGuess + "</p>" +
+      // "<p>Computer choose: " + computerGuess + "</p>" +
       "<p>Wins: " + wins + "</p>" +
       "<p>Loses: " + losses + "</p>" +
-      "<p>Number of Guesses Remaining: " + guessNumber + "</p>"
+      "<p>Number of Guesses Remaining: " + guessNumber + "</p>" +
+
       "<p>Letters you have already guessed: " + yourGuesses + "</p>";
 
       document.querySelector("#game").innerHTML = html;
+    }
 
-  }
-}
 
     
